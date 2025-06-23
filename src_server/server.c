4547a597a1054006234e5f75eff7d9e1592a2664
Miguel Monteiro <jmmonteiro@gmail.com>
2025-06-23 09:24:24 +0100

    20250623

--- src_server/server.c
+++ src_server/server.c
@@ -6,24 +6,28 @@
 /*   By: josemigu <josemigu@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 18:05:03 by josemigu          #+#    #+#             */
-/*   Updated: 2025/06/20 18:59:20 by josemigu         ###   ########.fr       */
+/*   Updated: 2025/06/21 13:24:39 by josemigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void signal_handler(int sig, siginfo_t *info, void *ucontext)
+{
+	
+}
+
 int	main(void)
 {
 	pid_t	pid;
-	struct sigaction sigact;
+	struct sigaction sa;
 	
 	pid = getpid();
 	ft_printf("%d\n", pid);
-	
+	ft_bzero(&sa, sizeof(sa));
+	sa.__sigaction_u.__sa_sigaction = &signal_handler;
+	sa.sa_flags = SA_SIGINFO;
 	while (true)
-	{
-		
-	}
-
+		pause();
 	return (EXIT_SUCCESS);
 }
